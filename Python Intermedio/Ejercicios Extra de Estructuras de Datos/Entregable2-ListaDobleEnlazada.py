5eb8b751535704f6f07e88a82132bc4f4096bddc
rommtraining <rommtraining@gmail.com>
2026-05-09 19:10:01 -0600

    Entregable extra estructura de datos

--- Python Intermedio/Ejercicios Extra de Estructuras de Datos/Entregable2-ListaDobleEnlazada.py
+++ Python Intermedio/Ejercicios Extra de Estructuras de Datos/Entregable2-ListaDobleEnlazada.py
@@ -11,54 +11,17 @@ class Node:
         self.previous = previous
         self.next = next
 
-class Doubly_Linked_List:
+class Linked_List:
     head : Node
-    tail : Node
 
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def _add_first_node(self,data):
-        new_node = Node(data)
-        self.head = new_node
-        self.tail = new_node
-    
-    def append(self, data):
-        if self.is_empty_list():
-            self._add_first_node(data)
-            return
-        current_node = self.tail
-        self.tail = Node(data)
-        current_node.next = self.tail
-        self.tail.previous = current_node
-
-
-    def prepend(self,data):
-        if self.is_empty_list():
-            self._add_first_node(data)
-            return
-        
-        current_node = self.head
-        self.head = Node(data)
-        current_node.previous = self.head
-        self.head.next = current_node
-
-    def are_head_and_tail_equal(self):
-        return self.head == self.tail
-    
-    def is_empty_list(self):
-        return self.head == None and self.tail == None
+    def __init__(self, head=None):
+        self.head = head
 
     def print_forward(self):
-        if self.is_empty_list():
+        if self.head == None:
             print("La lista esta vacia. No se puede imprimir.")
             return
         
-        if self.are_head_and_tail_equal():
-            print(self.head.data)
-            return
-
         current_node = self.head
 
         result = ""
@@ -72,13 +35,65 @@ class Doubly_Linked_List:
 
         print(result)
 
+class Doubly_Linked_List(Linked_List):
+    tail : Node
+
+    def __init__(self, head=None, tail=None):
+        super().__init__(head)
+        self.tail = tail
+
+    def _add_first_node(self,data):
+        self.head = Node(data)
+    
+    def append(self, data):
+        if self.is_empty_list():
+            self._add_first_node(data)
+            return
+        
+        new_node = Node(data)
+
+        if self.tail == None:
+            self.tail = new_node
+            self.head.next = new_node
+            self.tail.previous = self.head
+        else:
+            new_node.previous = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def prepend(self,data):
+        if self.is_empty_list():
+            self._add_first_node(data)
+            return
+        
+        new_node = Node(data)
+        current_node = self.head
+
+        if self.tail == None:
+            self.tail = self.head
+            self.tail.previous = new_node
+            self.head = new_node
+            self.head.next = self.tail
+        else:
+            self.head = new_node
+            current_node.previous = new_node
+            self.head.next = current_node
+
+    def head_exists_only(self):
+        return self.head != None and self.tail == None
+    
+    def tail_exists_only(self):
+        return self.head == None and self.tail != None
+    
+    def is_empty_list(self):
+        return self.head == None and self.tail == None
 
     def print_backward(self):
         if self.is_empty_list():
             print("La lista esta vacia. No se puede imprimir.")
             return
 
-        if self.are_head_and_tail_equal():
+        if self.are_head_exists_only():
             print(self.head.data)
             return
         
@@ -100,15 +115,22 @@ class Doubly_Linked_List:
             print("La lista esta vacia. No se puede eliminar.")
             return
         
-        if self.are_head_and_tail_equal():
+        if self.head_exists_only():
+            self.head = None
+            return
+        
+        if self.tail_exists_only():
+            self.tail = None
+            return
+        
+        if self.head == self.tail:
             self.head = None
             self.tail = None
             return
         
         #Si el head es igual que data, debe eliminarse, por lo que el nuevo head es el head.next y head.next.previous es None
         if self.head.data == data:
-            current_node = self.head.next
-            self.head == current_node
+            self.head = self.head.next
             self.head.previous = None
             return
         
@@ -118,18 +140,7 @@ class Doubly_Linked_List:
             self.tail = current_node
             return
         
-        #Si el head es diferente que data, se asigna el next a current_node para continuar buscando
-        current_node = self.head.next
-
-        #Si el current_node es el tail, quiere decir que la lista solo tiene dos nodos, por lo que se hace un manejo especial
-        #Si tail es igual que data, se debe eliminar el tail, por lo que el nuevo tail es el head
-        #if current_node == self.tail:
-        #    if self.tail.data == data:
-        #        self.tail = self.head
-        #        self.tail.previous = self.head
-        #        self.head.next = self.tail
-        #        self.head.previous = None
-        #        return
+        current_node = self.head
 
         while True:
             if current_node.data == data:
@@ -145,48 +156,58 @@ class Doubly_Linked_List:
 def input_value():
     return input("Ingrese el dato del nodo: ")
 
+def enter_to_continue():
+    input("Presione Enter para continuar.")
+
 def main():
 
     doubly_linked_list = Doubly_Linked_List()
+    print_control_list = True
 
     while True:
         clear_screen()
         opt = input(
 """
+----------------------------
 ----- Menú de Opciones -----
 ----------------------------
+D=Agregar al inicio
 F=Agregar al final
-I=Agregar al inicio
-D=Imprimir de izquierda a derecha
-Z=Imprimir derecha a izquierda
+E=Imprimir de izquierda a derecha
+R=Imprimir derecha a izquierda
 X=Eliminar
-S=Salir
+Q=Salir
 ----------------------------
 Ingrese la opcion: """).upper()
 
         match opt:
+            case "D":
+                doubly_linked_list.prepend(input_value())
             case "F":
                 doubly_linked_list.append(input_value())
-            case "I":
-                doubly_linked_list.prepend(input_value())
-            case "D":
+            case "E":
                 doubly_linked_list.print_forward()
-            case "Z":
+                print_control_list = False
+            case "R":
                 doubly_linked_list.print_backward()
+                print_control_list = False
             case "X":
                 if doubly_linked_list.is_empty_list():
                     print("La lista esta vacia. No se puede eliminar.")
+                    enter_to_continue()
                     continue
                 doubly_linked_list.delete(input_value())
-            case "S":
+                print_control_list = True
+            case "Q":
                 print("Saliendo del sistema")
                 break
             case _:
                 print("Opcion invalida. Intente de nuevo.")
                 continue
         
-        doubly_linked_list.print_forward()
-        input("Presione Enter para continuar.")
+        if print_control_list:
+            doubly_linked_list.print_forward()
+        enter_to_continue()
 
 if __name__ == "__main__":
     main()
